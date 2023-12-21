74974953d9b92cfe77ad2f5dd36325a4433d927b
bdgee <bdgee925@gmail.com>
2023-12-21 11:26:17 -0800

    Update test.py

--- Code/test.py
+++ Code/test.py
@@ -1,10 +1,16 @@
 import time
-import pyfirmata
+from pyfirmata import *
 
 if __name__ == '__main__':
 	board = pyfirmata.Arduino('/dev/ttyACM0')
 	print("Success")
+
+	echo = board.get_pin('d:11:i')
+	trig = board.get_pin('d:12:o')
+	LED = board.get_pin('d:13:o')
 	
+	
+
 	while True:
 		board.digital[13].write(1)
 		time.sleep(1)
