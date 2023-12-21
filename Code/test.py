944ae6f34804240e6b4c62d609cbcf1d2db288e1
bdgee <bdgee925@gmail.com>
2023-12-21 13:39:27 -0800

    Update test.py

--- Code/test.py
+++ Code/test.py
@@ -1,16 +1,10 @@
 import time
-from pymata4 import pymata4
+from pyfirmata import *
 
-def distance(data):
-	print(data[2])
+if __name__ == '__main__':
+	board = Arduino('/dev/ttyACM0')
+	print("Success")
 
-#board = Arduino('/dev/ttyACM0')
-board = pymata4.Pymata4()
-
-trig = 11
-echo = 12
-board.set_pin_mode_sonar(trig, echo, distance)
-
-while True: 
-	time.sleep(.5)
-	board.sonar_read(trig)
+	while True:
+		print('pain')
+		time.sleep(1)
