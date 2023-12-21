a3e92bf42837641f22b0ce7466c73e778e44c070
bdgee <bdgee925@gmail.com>
2023-12-21 13:14:52 -0800

    brh

--- Code/test.py
+++ Code/test.py
@@ -5,6 +5,8 @@ if __name__ == '__main__':
 	board = Arduino('/dev/ttyACM0')
 	print("Success")
 
+	board.analog[0].enable_reporting()
+
 	while True:
 		print(board.analog[0].read())
 		time.sleep(1)
