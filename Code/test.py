09d814dc68124b4ceb03b36b1b84f452b23275ad
bdgee <bdgee925@gmail.com>
2023-12-21 13:09:38 -0800

    Update test.py

--- Code/test.py
+++ Code/test.py
@@ -5,7 +5,6 @@ if __name__ == '__main__':
 	board = Arduino('/dev/ttyACM0')
 	print("Success")
 
-	with board:
-		while True:
-			print(board.analog[0].read())
-			time.sleep(1)
+	while True:
+		print(board.analog[0].read())
+		time.sleep(1)
