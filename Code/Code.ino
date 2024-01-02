141e04301ccfc5e85151a4559921d615a4c57cbe
bdgee <bdgee925@gmail.com>
2024-01-01 20:34:47 -0800

    bruh

--- Code/Code.ino
+++ Code/Code.ino
@@ -1,3 +1,9 @@
+#include <Servo.h>
+
+Servo myservo;
+
+int pos = 0;    // variable to store the servo position
+
 // defines pins numbers
 const int trigPin = 9;
 const int echoPin = 10;
@@ -8,6 +14,8 @@ void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   Serial.begin(9600); // Starts the serial communication
+
+  myservo.attach(3);  // attaches the servo on pin 9 to the servo object
 }
 void loop() {
   // Clears the trigPin
@@ -24,4 +32,15 @@ void loop() {
   // Prints the distance on the Serial Monitor
   Serial.print("Distance: ");
   Serial.println(distance);
+
+
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
 }
