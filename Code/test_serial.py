2905c6b0b6aa57a7331206238114d32ebefdd257
bdgee <bdgee925@gmail.com>
2024-01-14 16:01:44 -0800

    Update test_serial.py

--- Code/test_serial.py
+++ Code/test_serial.py
@@ -1,14 +1,26 @@
 import serial.tools.list_ports
+import time
 
-ports = serial.tools.list_ports.comports()
 serialInst = serial.Serial()
 
-portVar = '/dev/tty/ACM0'
-serialInst.baudrate = 9600
-serialInst.port = portVar
-serialInst.open()
+# portVar = '/dev/tty/ACM0'
+portVar = 'COM3'
+serialInst = serial.Serial(portVar, 115200, timeout=1.0)
+time.sleep(3)
+serialInst.reset_input_buffer()
+print("Serial OK")
+n = 1
 
-while True:
-    if serialInst.in_waiting:
-        packet = serialInst.readline()
-        print(packet.decode('utf').rstrip('\n'))
+try:
+    while True:
+        time.sleep(.01)
+        #Send message
+        serialInst.write((str(n+1)+"\n").encode('utf-8'))
+        while serialInst.in_waiting <= 0:
+            time.sleep(.01)
+        packet = serialInst.readline().decode('utf-8')
+        n = int(packet)
+        print(n)
+except KeyboardInterrupt:
+    print("Close Serial communication")
+    serialInst.close()
