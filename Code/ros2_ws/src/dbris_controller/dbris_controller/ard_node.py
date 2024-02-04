24039571d698b9cbc628d7cd8aa99c0fdde0b82a
Brandon Gee <bdgee925@gmail.com>
2024-02-04 11:45:44 -0800

    FKJDSKLFJSLDFJkl

---
+++ Code/ros2_ws/src/dbris_controller/dbris_controller/ard_node.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float64MultiArray
+
+class ArdNode(Node):
+
+    def __init__(self):
+        super().__init__("ard_node")
+        self.get_logger().info("ard_node has been started")
+
+        #Subscribers
+        self.dir_sub_ = self.create_subscription(Float64MultiArray, "robot_dir", self.sub_robot_dir, 10)
+
+        #Publishers
+        self.motor_pos_pub_ = self.create_publisher(Float64MultiArray, "motor_pos", 10)
+        self.timer_ = self.create_timer(1.0/5.0, self.pub_motor_pos)
+    
+    def pub_motor_pos(self):
+        msg = Float64MultiArray()
+
+        #Get the motor position
+        msg.data = [0.0, 0.0]
+        self.motor_pos_pub_.publish(msg)
+
+    def sub_robot_dir(self, msg:Float64MultiArray):
+        self.get_logger().info(str(msg))
+
+
+def main (args = None):
+    rclpy.init(args = args)
+    node = ArdNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
---
+++ Code/ros2_ws/src/dbris_controller/dbris_controller/raspi_node.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import *
+
+class RaspiNode(Node):
+
+    def __init__(self):
+        super().__init__("raspi_node") 
+        self.get_logger().info("raspi_node has been started")
+
+        #Publishers
+        self.dir_pub_ = self.create_publisher(Float64MultiArray, "robot_dir", 10)
+        self.timer_ = self.create_timer(1.0/5.0, self.pub_robot_dir)
+        
+        #Subscribers
+        self.motor_pos_sub = self.create_subscription(Float64MultiArray, "motor_pos", self.sub_motor_pos, 10)
+
+    def pub_robot_dir (self, msg:Float64MultiArray):
+        msg.data = [1.0, 1.0]
+        self.dir_pub_.publish(msg)
+
+    def sub_motor_pos (self, msg:Float64MultiArray):
+        self.get_logger().info(str(msg))
+
+def main (args = None):
+    rclpy.init(args = args)
+    node = RaspiNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
